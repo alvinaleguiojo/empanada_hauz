68566e69aee7a9a6e795b31c2daf2859a5a5252d
alvinaleguiojo <74090064+alvinaleguiojo@users.noreply.github.com>
2026-08-21 23:32:51 +0800

    fix(rider): configure Google Maps native API keys

--- apps/rider/app.config.js
+++ apps/rider/app.config.js
@@ -1,13 +1,36 @@
 module.exports = ({ config }) => ({
   ...config,
   android: {
-    ...config.android,
+    ...(config.android || {}),
     config: {
-      ...config.android?.config,
+      ...(config.android?.config || {}),
       googleMaps: {
-        ...config.android?.config?.googleMaps,
-        apiKey: process.env.GOOGLE_MAPS_API_KEY,
+        ...(config.android?.config?.googleMaps || {}),
+        apiKey:
+          process.env.GOOGLE_MAPS_ANDROID_API_KEY ||
+          process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY ||
+          process.env.GOOGLE_MAPS_API_KEY ||
+          "",
       },
     },
+  },
+  ios: {
+    ...(config.ios || {}),
+    config: {
+      ...(config.ios?.config || {}),
+      googleMapsApiKey:
+        process.env.GOOGLE_MAPS_IOS_API_KEY ||
+        process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY ||
+        process.env.GOOGLE_MAPS_API_KEY ||
+        "",
+    },
+  },
+  extra: {
+    ...(config.extra || {}),
+    googleMapsDirectionsApiKey:
+      process.env.EXPO_PUBLIC_GOOGLE_DIRECTIONS_API_KEY ||
+      process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY ||
+      process.env.GOOGLE_MAPS_API_KEY ||
+      "",
   },
 });
