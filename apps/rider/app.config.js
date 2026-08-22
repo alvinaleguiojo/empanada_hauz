4e33c46f3b9d86c6ff38bd0142719541f999a5cc
alvinaleguiojo <74090064+alvinaleguiojo@users.noreply.github.com>
2026-08-22 11:00:21 +0800

    Fix rider Google Maps native API key configuration

--- apps/rider/app.config.js
+++ apps/rider/app.config.js
@@ -1,37 +1,53 @@
-module.exports = ({ config }) => ({
-  ...config,
-  android: {
-    ...(config.android || {}),
-    config: {
-      ...(config.android?.config || {}),
-      googleMaps: {
-        ...(config.android?.config?.googleMaps || {}),
-        apiKey:
-          process.env.GOOGLE_MAPS_ANDROID_API_KEY ||
-          process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY ||
-          process.env.GOOGLE_MAPS_API_KEY ||
-          "",
+module.exports = ({ config }) => {
+  const googleMapsApiKey =
+    process.env.GOOGLE_MAPS_ANDROID_API_KEY ||
+    process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY ||
+    process.env.GOOGLE_MAPS_API_KEY ||
+    "";
+
+  const plugins = (config.plugins || []).filter((plugin) => {
+    const name = Array.isArray(plugin) ? plugin[0] : plugin;
+    return name !== "react-native-maps";
+  });
+
+  plugins.push([
+    "react-native-maps",
+    {
+      androidGoogleMapsApiKey: googleMapsApiKey,
+      iosGoogleMapsApiKey:
+        process.env.GOOGLE_MAPS_IOS_API_KEY || googleMapsApiKey,
+    },
+  ]);
+
+  return {
+    ...config,
+    plugins,
+    android: {
+      ...(config.android || {}),
+      config: {
+        ...(config.android?.config || {}),
+        googleMaps: {
+          ...(config.android?.config?.googleMaps || {}),
+          apiKey: googleMapsApiKey,
+        },
       },
     },
-  },
-  ios: {
-    ...(config.ios || {}),
-    config: {
-      ...(config.ios?.config || {}),
-      googleMapsApiKey:
-        process.env.GOOGLE_MAPS_IOS_API_KEY ||
+    ios: {
+      ...(config.ios || {}),
+      config: {
+        ...(config.ios?.config || {}),
+        googleMapsApiKey:
+          process.env.GOOGLE_MAPS_IOS_API_KEY || googleMapsApiKey,
+      },
+    },
+    extra: {
+      ...(config.extra || {}),
+      googleMapsDirectionsApiKey:
+        process.env.EXPO_PUBLIC_GOOGLE_ROUTES_API_KEY ||
+        process.env.EXPO_PUBLIC_GOOGLE_DIRECTIONS_API_KEY ||
         process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY ||
         process.env.GOOGLE_MAPS_API_KEY ||
         "",
     },
-  },
-  extra: {
-    ...(config.extra || {}),
-    googleMapsDirectionsApiKey:
-      process.env.EXPO_PUBLIC_GOOGLE_ROUTES_API_KEY ||
-      process.env.EXPO_PUBLIC_GOOGLE_DIRECTIONS_API_KEY ||
-      process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY ||
-      process.env.GOOGLE_MAPS_API_KEY ||
-      "",
-  },
-});
+  };
+};
