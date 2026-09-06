c8a3e3cfe382abefb0acc3d4e5a4142f6ca8abc8
alvinaleguiojo <74090064+alvinaleguiojo@users.noreply.github.com>
2026-09-06 20:09:24 +0800

    Register Messenger order routing guard

--- apps/api/src/modules/messenger/messenger.module.ts
+++ apps/api/src/modules/messenger/messenger.module.ts
@@ -8,11 +8,12 @@ import { MessengerController } from "./messenger.controller";
 import { MessengerService } from "./messenger.service";
 import { MetaAuthService } from "./meta-auth.service";
 import { MessengerSyncService } from "./messenger-sync.service";
+import { MessengerOrderRoutingGuardService } from "./messenger-order-routing-guard.service";
 
 @Module({
   imports: [AiModule, CustomersModule, OrdersModule, McpModule, NotificationsModule],
   controllers: [MessengerController],
-  providers: [MessengerService, MetaAuthService, MessengerSyncService],
+  providers: [MessengerService, MetaAuthService, MessengerSyncService, MessengerOrderRoutingGuardService],
   exports: [MessengerService, MetaAuthService]
 })
 export class MessengerModule {}
