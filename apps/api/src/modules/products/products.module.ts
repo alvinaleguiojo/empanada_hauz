8d79baad020a0c284398cc9a881baf7ab9b549a0
alvinaleguiojo <74090064+alvinaleguiojo@users.noreply.github.com>
2026-09-09 17:31:29 +0800

    feat(products): connect uploads to documents

--- apps/api/src/modules/products/products.module.ts
+++ apps/api/src/modules/products/products.module.ts
@@ -1,10 +1,11 @@
 import { Module } from "@nestjs/common";
 import { DatabaseModule } from "../../database/database.module";
+import { DocumentsModule } from "../documents/documents.module";
 import { ProductsController } from "./products.controller";
 import { ProductsService } from "./products.service";
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, DocumentsModule],
   controllers: [ProductsController],
   providers: [ProductsService],
   exports: [ProductsService]
