22acb48e649f9b65fde8da91fed94463bdf47b69
alvinaleguiojo <74090064+alvinaleguiojo@users.noreply.github.com>
2026-09-11 13:04:24 +0800

    feat(api): register free Cebuano TTS module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -27,13 +27,14 @@ import { AdminUsersModule } from "./modules/admin-users/admin-users.module";
 import { ProductsModule } from "./modules/products/products.module";
 import { DocumentsModule } from "./modules/documents/documents.module";
 import { TranscriptionModule } from "./modules/transcription/transcription.module";
+import { TtsModule } from "./modules/tts/tts.module";
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }), DatabaseModule, RealtimeModule, HealthModule, AuthModule, AdminUsersModule,
     ProductsModule, DocumentsModule, MessengerModule, AiModule, AiInstructionsModule, CustomersModule, OrdersModule, BatchesModule, KitchenModule,
     DeliveriesModule, DeliveryNetworkModule, RiderModule, AnalyticsModule, InventoryModule, ExpensesModule, ReferralsModule,
-    ReferralChatModule, NotificationsModule, ChatModule, McpModule, GoogleMapsModule, TranscriptionModule
+    ReferralChatModule, NotificationsModule, ChatModule, McpModule, GoogleMapsModule, TranscriptionModule, TtsModule
   ]
 })
 export class AppModule {}
